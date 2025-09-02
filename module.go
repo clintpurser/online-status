58a0bf35366b0275e1da2636a4a0e633428cc72a
Clint Purser <clint.purser@viam.com>
2025-09-02 16:15:05 -0400

    Update module.go

--- module.go
+++ module.go
@@ -23,62 +23,30 @@ func init() {
 	)
 }
 
-type Config struct {
-	/*
-		Put config attributes here. There should be public/exported fields
-		with a `json` parameter at the end of each attribute.
-
-		Example config struct:
-			type Config struct {
-				Pin   string `json:"pin"`
-				Board string `json:"board"`
-				MinDeg *float64 `json:"min_angle_deg,omitempty"`
-			}
-
-		If your model does not need a config, replace *Config in the init
-		function with resource.NoNativeConfig
-	*/
-}
-
-// Validate ensures all parts of the config are valid and important fields exist.
-// Returns implicit required (first return) and optional (second return) dependencies based on the config.
-// The path is the JSON path in your robot's config (not the `Config` struct) to the
-// resource being validated; e.g. "components.0".
-func (cfg *Config) Validate(path string) ([]string, []string, error) {
-	// Add config validation code here
-	return nil, nil, nil
-}
-
 type onlineStatusOnlineStatus struct {
 	resource.AlwaysRebuild
 
 	name resource.Name
 
 	logger logging.Logger
-	cfg    *Config
 
 	cancelCtx  context.Context
 	cancelFunc func()
 }
 
 func newOnlineStatusOnlineStatus(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (sensor.Sensor, error) {
-	conf, err := resource.NativeConfig[*Config](rawConf)
-	if err != nil {
-		return nil, err
-	}
 
-	return NewOnlineStatus(ctx, deps, rawConf.ResourceName(), conf, logger)
+	return NewOnlineStatus(ctx, deps, rawConf.ResourceName(), resource.NoNativeConfig{}, logger)
 
 }
 
-func NewOnlineStatus(ctx context.Context, deps resource.Dependencies, name resource.Name, conf *Config, logger logging.Logger) (sensor.Sensor, error) {
+func NewOnlineStatus(ctx context.Context, deps resource.Dependencies, name resource.Name, _ resource.NoNativeConfig, logger logging.Logger) (sensor.Sensor, error) {
 
 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
 
 	s := &onlineStatusOnlineStatus{
 		name:       name,
 		logger:     logger,
-		cfg:        conf,
 		cancelCtx:  cancelCtx,
 		cancelFunc: cancelFunc,
 	}
